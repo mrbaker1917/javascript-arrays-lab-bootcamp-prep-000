dada583ea3a3d9b561731831ed4eac89bd18d173
Mark Baker <mrbaker1917@gmail.com>
2018-07-18 03:22:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Add your functions and code here
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppends
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+destructivelyAppendKitten('Ralph');
